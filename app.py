ff31223c54f6ebdb7bcc84431e30279dda1f566d
sibims <sibima00@gmail.com>
2024-05-06 09:58:09 +0530

    created the rag chain and retriever chain

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.llms import HuggingFaceHub
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from sentence_transformers import SentenceTransformer
-
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+# from sentence_transformers import SentenceTransformer
+# from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms import Ollama
 
 def get_vectorstore_from_url(url):
     loader = WebBaseLoader(url)
@@ -23,6 +26,56 @@ def get_vectorstore_from_url(url):
     vector_store = Chroma.from_documents(document_chunks,embeddings)
 
     return vector_store
+
+
+
+def get_context_retriever_chain(vector_store):
+    llm = Ollama(
+        model="phi3",
+        verbose=True,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    )
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+    
+def get_conversational_rag_chain(retriever_chain): 
+    llm = Ollama(
+        model="phi3",
+        verbose=True,
+        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    )
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    
+    return response['answer']
 
 st.set_page_config(page_title="YarnitCaseStudy", page_icon="🤖")
 st.title("YarnitCaseStudy")
@@ -43,3 +96,17 @@ else:
         ]
     if "vector_store" not in st.session_state:
         st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+    # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
